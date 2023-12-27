b44ca66b7002e1c39a1ff79c62bb02b1bc89657c
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-12-27 07:52:40 -0700

    Create 22.sh

---
+++ 22.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Your Git login information
+git_username="your-username"
+git_password="your-password"
+git_site="your-git-site"
+
+# Save the login information to a file
+echo "Username: $git_username" > ~/Documents/git-login-info.txt
+echo "Password: $git_password" >> ~/Documents/git-login-info.txt
+echo "Site: $git_site" >> ~/Documents/git-login-info.txt
